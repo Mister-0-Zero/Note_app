34e72600c030693c25614981124196087a8e2e9c
Mister-0-Zero <alexyulyadkov@gmail.com>
2024-09-30 23:11:21 +0300

    adding buttom 'add_notes'

--- practic/Notes.py
+++ practic/Notes.py
@@ -1,5 +1,5 @@
 import flet as fl
-from flet import AppBar, Text, Container
+from flet import AppBar, Text, Container, Row, Stack, Column
 
 
 class Note(Container):
@@ -7,7 +7,7 @@ class Note(Container):
         super().__init__(
             height=200,
             width=200,
-            padding=10,  # Задаем конкретное значение для padding
+            padding=10,
             border_radius=10,
             border=fl.border.all(1, "black"),
             bgcolor="#407375",
@@ -16,19 +16,69 @@ class Note(Container):
                 Text(content, overflow="clip")
             ],
             alignment="start",
-            spacing=10  # Используем пиксели для spacing
+            spacing=10
             ),
-            alignment=fl.alignment.center  # Исправлено написание alignment
+            alignment=fl.alignment.center
         )
 
 
 def main(page: fl.Page):
+    # Список для хранения всех заметок
+    notes = []
+
+
     page.title = "Notes"
     page.appbar = AppBar(
         title=Text("Заметки", size=24),
         center_title=True,
     )
-    page.add(Note("Первая заметка", "Это невероятно, я наконец-то начал писать интерфейсы, а раньше понять не мог как их делают, ема круто!"))
+
+    # Контейнер для заметок с прокруткой
+    notes_container = Row(wrap=True, spacing=10)
+
+    # Обертываем контейнер заметок в Column для добавления прокрутки
+    scrollable_container = Column(
+        controls=[notes_container],
+        expand=True,
+        scroll="auto"  # Включаем прокрутку, когда содержимое превышает высоту экрана
+    )
+
+    # Функция для добавления новой заметки
+    def add_note(e):
+        new_note = Note("Новая заметка", "Текст заметки")
+        notes.append(new_note)
+        notes_container.controls.append(new_note)
+        page.update()
+
+    def open_set():
+        pass
+    # Кнопка добавления заметки
+    add_button = fl.FloatingActionButton(
+        icon=fl.icons.ADD,
+        on_click=add_note
+    )
+    setting_buttom = fl.FloatingActionButton(
+        icon = fl.icons.SETTINGS,
+        on_click=open_set
+    )
+
+    # Создаем главный контейнер Stack, который будет содержать заметки и кнопку
+    page.add(
+        Stack([
+            # Контейнер с заметками
+            Container(
+                content=scrollable_container,
+                expand=True  # Растягиваем на весь экран
+            ),
+            # Фиксируем кнопку в правом нижнем углу
+            Container(
+                content=add_button,
+                alignment=fl.alignment.bottom_right,  # Выравниваем кнопку в нижнем правом углу
+                margin=fl.margin.all(16),
+            ),
+        ])
+    )
+
     page.update()
 
 fl.app(target=main)
